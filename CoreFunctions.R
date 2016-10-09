aa97eda0288d1a432851ff5c36b2a28fc80e02a7
SkBlaz <skrljblaz@gmail.com>
2016-10-09 10:50:52 +0200

    Working heatmap tool

--- CoreFunctions.R
+++ CoreFunctions.R
@@ -18,6 +18,8 @@ inputfile <- args[1]
 percentIdentical <- args[2]
 outfile <- args[3]
 
+
+
 ## make color scheme according to 2
 ## construct a custom heatmap, which can easily plot different percentages .. according to
 ## for each, which count with apply(1) < percentidentical, color differently, x - x/2 color differnetly and fianlly
@@ -27,8 +29,12 @@ outfile <- args[3]
 
 ## end.
 
-# palette(rainbow(6))
-# colorRampPalette(c("blue", "red"))( 4) 
+                                        # palette(rainbow(6))
+                                        # colorRampPalette(c("blue", "red"))( 4) 
+
+
+set.seed(88)
+
 
 if (!require("gplots")) {
     install.packages("gplots", dependencies = TRUE)
@@ -39,54 +45,43 @@ if (!require("RColorBrewer")) {
     library(RColorBrewer)
 }
 
-data <- read.csv(inputfile, comment.char="#")
-rnames <- data[,1]                            # assign labels in column 1 to "rnames"
-mat_data <- data.matrix(data[,2:ncol(data)])  # transform column 2-5 into a matrix
-rownames(mat_data) <- rnames                  # assign row names
 
-                                        # creates a own color palette from red to green
+### testplot
+data <- read.csv(paste("data/",inputfile,sep=""), comment.char="#")
+rnames <- data[,1]                            # assign labels in column 1 to "rnames"
+mat_data <- data.matrix(data[2:nrow(data),2:ncol(data)])  # transform column 2-5 into a matrix
+                                        #rownames(mat_data) <- rnames                  # assign row names
+
+print ("Filtering out unimportant rows")
+freqtable <- sort(table(apply(mat_data,1,sum)))
+threshold <- as.numeric(names(freqtable)[length(names(freqtable))])
+uninportant <- which(apply(mat_data,1,sum)==threshold)
+
+fram3 <- mat_data[-uninportant,]
+
+print (paste("New dataframe consists of", nrow(fram3), " rows."))
+print("Drawing heatmap!")
+## heatmap(fram3, Rowv=NA, Colv=NA, col = topo.colors(512), scale="column", margins=c(5,10))
+print("Heatmaps are generated in the PDF file.")
+
+##                                         # creates a own color palette from red to green
 my_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
 
-                                        # (optional) defines the color breaks manually for a "skewed" color transition
-col_breaks = c(seq(-1,0,length=100),  # for red
-               seq(0.01,0.8,length=100),           # for yellow
-               seq(0.81,1,length=100))             # for green
-
-                                        # creates a 5 x 5 inch image
-
-if (outfile != null)}{
-    png(paste("slike/",outfile),    # create PNG for the heat map        
-        width = 5*300,        # 5 x 300 pixels
-        height = 5*300,
-        res = 300,            # 300 pixels per inch
-        pointsize = 8)        # smaller font size
-
-    heatmap.2(mat_data,
-              cellnote = mat_data,  # same data set for cell labels
-              main = "Correlation", # heat map title
-              notecol="black",      # change font color of cell labels to black
-              density.info="none",  # turns off density plot inside color legend
-              trace="none",         # turns off trace lines inside the heat map
-              margins =c(12,9),     # widens margins around plot
-              col=my_palette,       # use on color palette defined earlier
-              breaks=col_breaks,    # enable color transition at specified limits
-              dendrogram="row",     # only draw a row dendrogram
-              Colv="NA")            # turn off column clustering
-
-    dev.off()  
-}else{
-
-    heatmap.2(mat_data,
-              cellnote = mat_data,  # same data set for cell labels
-              main = "Correlation", # heat map title
-              notecol="black",      # change font color of cell labels to black
-              density.info="none",  # turns off density plot inside color legend
-              trace="none",         # turns off trace lines inside the heat map
-              margins =c(12,9),     # widens margins around plot
-              col=my_palette,       # use on color palette defined earlier
-              breaks=col_breaks,    # enable color transition at specified limits
-              dendrogram="row",     # only draw a row dendrogram
-              Colv="NA")            # turn off column clustering
+##                                         # (optional) defines the color breaks manually for a "skewed" color transition
+col_breaks = c(seq(1,2,length=100),  # for red
+               seq(2.01,3,length=100),        
+               seq(3.01,4,length=100))                                                     # creates a 5 x 5 inch image
 
 
-}
+heatmap.2(fram3,
+          cellnote = fram3,  # same data set for cell labels
+          main = "Heatmap!", # heat map title
+          notecol="black",      # change font color of cell labels to black
+          density.info="none",  # turns off density plot inside color legend
+          trace="none",         # turns off trace lines inside the heat map
+          symkey=F,
+          margins =c(12,9),     # widens margins around plot  
+          breaks=col_breaks,    # enable color transition at specified limits
+          dendrogram="row",     # only draw a row dendrogram
+          Colv="NA",col=my_palette)      # use on color palette defined earlier)            # turn off column clustering
+
